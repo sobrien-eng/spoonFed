fd919f72e9d8d43c91f1c8365908b124ee3811da
Daijah <daijahlinyahcoppage@gmail.com>
2023-04-12 08:41:18 -0600

    pages
    signup and user profile skeleton

---
+++ spoonfed/src/Components/AccProfile.js
@@ -0,0 +1,16 @@
+import styles from '../Styles/AccProfile.css';
+const AccountProfile = () => {
+    return (
+        <>
+        <h4>Account Info</h4>
+        <div id='box'>
+            <p>saved</p>
+            <p>liked</p>
+            <p>created</p>
+        </div>
+        <div><h5>update/delete account</h5></div>
+        </>
+    );
+}
+
+export default AccountProfile;
---
+++ spoonfed/src/Components/NavBar.js
@@ -0,0 +1,14 @@
+import styles from '../Styles/NavBar.css'; 
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Account</a></li>
+                <li><a href="#">Recipes</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
