74488bc658cfc3caa923216e44f32482f9d2323f
Daijah <daijahlinyahcoppage@gmail.com>
2023-04-12 23:01:02 -0600

    more styling

--- spoonfed/src/Components/AccProfile.js
+++ spoonfed/src/Components/AccProfile.js
@@ -3,10 +3,31 @@ const AccountProfile = () => {
     return (
         <>
         <h4>Account Info</h4>
-        <div id='box'>
-            <p>saved</p>
-            <p>liked</p>
-            <p>created</p>
+        <div id='container'>
+            <div id='saved'>
+                <label>Your saved recipes: </label>
+                <ul>
+                    <li><a href="#">Chicken Nachos</a></li>
+                    <li><a href="#">Mac 'n' Cheese</a></li>
+                    <li><a href="#">Sushi</a></li>
+                </ul>
+            </div>
+            <div id="liked">
+                <label>Your liked recipes: </label>
+                <ul>
+                    <li><a href="#">Chicken Nachos</a></li>
+                    <li><a href="#">Pesto Spaghetti</a></li>
+                    <li><a href="#">Potato Salad</a></li>
+                </ul>
+            </div>
+            <div id="create">
+                <label>Recipes you've created: </label>
+                <ul>
+                    <li><a href="#">Aunt Carol's Cassorole</a></li>
+                    <li><a href="#">Tomato fries</a></li>
+                    <li><a href="#">Quail Omlette</a></li>
+                </ul>
+            </div>
         </div>
         </>
     );
