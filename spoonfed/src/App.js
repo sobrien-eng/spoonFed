f61e0c038de9e101ed459059de33aab457c7b968
Daijah <daijahlinyahcoppage@gmail.com>
2023-04-13 00:45:06 -0600

    skeleton of account and nav bar.

--- spoonfed/src/App.js
+++ spoonfed/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import AccountProfile from './Components/AccProfile';
 import NavBar from './Components/NavBar';
-import Signup from './Components/Signup';
+import Home from './Components/Home';
 
 function App() {
   return (
     <>
     <NavBar />
-    <AccountProfile />
+    <Home />
+    {/*<AccountProfile />*/}
     </>
   );
 }
--- spoonfed/src/Components/AccProfile.js
+++ spoonfed/src/Components/AccProfile.js
@@ -3,10 +3,31 @@ const AccountProfile = () => {
     return (
         <>
         <h4>Account Info</h4>
-        <div id='box'>
-            <p>saved</p>
-            <p>liked</p>
-            <p>created</p>
+        <div id='container'>
+            <div id="saved">
+                <label>Your saved recipes: </label>
+                <ul>
+                    <li><a href="#">Chicken Soup</a></li>
+                    <li><a href="#">Turkey Burgers</a></li>
+                    <li><a href="#">Gumbo</a></li>
+                </ul>
+            </div>
+            <div id="liked">
+            <label>Your liked recipes: </label>
+                <ul>
+                    <li><a href="#">Quail Omlette</a></li>
+                    <li><a href="#">Blueberry Pie</a></li>
+                    <li><a href="#">Burrito</a></li>
+                </ul>
+            </div>
+            <div id="created">
+            <label>Recipes you've created: </label>
+                <ul>
+                    <li><a href="#">Grill Cheese</a></li>
+                    <li><a href="#">Pesto</a></li>
+                    <li><a href="#">Crepes</a></li>
+                </ul>
+            </div>
         </div>
         </>
     );
