6504da82d33c287ff07a18038a97fe363a88b0db
JeremieCius <jeremiecius@gmail.com>
2023-04-23 22:47:47 -0600

    Some api code for recipes

--- amplify/backend/function/recipes/src/app.js
+++ amplify/backend/function/recipes/src/app.js
@@ -5,9 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License"). You may not use 
 or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and limitations under the License.
 */
-
-
-
+const mysql = require("mysql");
 
 const express = require('express')
 const bodyParser = require('body-parser')
@@ -25,18 +23,31 @@ app.use(function(req, res, next) {
   next()
 });
 
+const db = mysql.createConnection({
+  host: "spoonfed2.cap3bgu68rku.us-west-2.rds.amazonaws.com",
+  user: "admin",
+  password: "spoonFed!1",
+  port: "3306",
+  database: "spoonFed"
+})
 
+db.connect();
 /**********************
  * Example get method *
  **********************/
 
 app.get('/recipes', function(req, res) {
   // Add your code here
+  db.query('SELECT * FROM RecipeInfo', function (error, results, fields) {
+    if (error) throw error;
+    res.json(results);
+  });
   res.json({success: 'get call succeed!', url: req.url});
 });
 
 app.get('/recipes/*', function(req, res) {
   // Add your code here
+  
   res.json({success: 'get call succeed!', url: req.url});
 });
 
@@ -46,6 +57,16 @@ app.get('/recipes/*', function(req, res) {
 
 app.post('/recipes', function(req, res) {
   // Add your code here
+  const recipe = req.body
+  
+  // execute a query to insert the new recipe into the RecipeInfo table
+  db.query('INSERT INTO RecipeInfo SET ?', recipe, function (error, results, fields) {
+    if (error) {
+      res.json({error: 'post call failed!', message: error})
+    } else {
+      res.json({success: 'post call succeed!', data: {id: results.insertId}});
+    }
+  })
   res.json({success: 'post call succeed!', url: req.url, body: req.body})
 });
 
@@ -65,7 +86,16 @@ app.put('/recipes', function(req, res) {
 
 app.put('/recipes/*', function(req, res) {
   // Add your code here
-  res.json({success: 'put call succeed!', url: req.url, body: req.body})
+  const recipe = req.body
+  db.query('UPDATE RecipeInfo SET ? WHERE id = ?', [recipe, req.params.id], function (error, results, fields) {
+    if (error) {
+      res.json({error: 'put call failed!', message: error})
+    } else if (results.affectedRows === 0) {
+      res.status(404).json({error: 'recipe not found'})
+    } else {
+      res.json({success: 'put call succeed!'});
+    }
+  })
 });
 
 /****************************
@@ -77,9 +107,16 @@ app.delete('/recipes', function(req, res) {
   res.json({success: 'delete call succeed!', url: req.url});
 });
 
-app.delete('/recipes/*', function(req, res) {
-  // Add your code here
+app.delete('/recipes/:id', function(req, res) {
+  db.query('DELETE FROM RecipeInfo WHERE id = ?', [req.params.id], function (error, results, fields) {
+    if (error) {
+      res.json({error: 'delete call failed!', message: error})
+    } else if (results.affectedRows === 0) {
+      res.status(404).json({error: 'recipe not found'})
+    } else {
   res.json({success: 'delete call succeed!', url: req.url});
+  }
+  })
 });
 
 app.listen(3000, function() {
