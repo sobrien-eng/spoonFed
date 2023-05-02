4e023a2b6453e060df4abf6aab2d3600090efb32
sobrien-eng <sobrien@student.neumont.edu>
2023-05-02 08:09:10 -0600

    +

--- spoonfed/src/App.js
+++ spoonfed/src/App.js
@@ -6,13 +6,51 @@ import Home from './Components/Home';
 import Login from './Components/Login';
 import Register from './Components/Register';
 import Status from './Components/Status';
+
+const apiURL = "https://localhost3000/recipes";
+ 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+  const [query, setQuery] = useState('');
+
+
+  useEffect(() => {
+		fetch(apiURL)
+			.then((res) => res.json())
+			.then(data => {
+			
+				setMovies(data.results);
+			})
+	}, [])
+
+  const searchRecipe = async (e) => {
+		e.preventDefault();
+		console.log("Searching");
+		try {
+			const url = `https://localhost3000/recipes/query=${query}`;
+			const res = await fetch(url);
+			const data = await res.json();
+		
+
+			console.log("data.results");
+			setMovies(data.results);
+		
+		
+		}
+		catch (e) {
+			console.log(e);
+		}
+	}
+  const changeHandler = (e) => {
+		setQuery(e.target.value);
+	}
   return (
     // <>
     // <NavBar />
     // <Home />
     // {/*<AccountProfile />*/}
     // </>
+
     <Account>
       <Status />
       <BrowserRouter>
